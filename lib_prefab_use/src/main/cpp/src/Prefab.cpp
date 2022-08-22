56630c10ad53fdc33607a9847b64c25c387791be
peerless2012 <peerless2012@126.com>
2022-08-23 00:06:27 +0800

    Update prefab use demo text.

--- lib_prefab_use/src/main/cpp/src/Prefab.cpp
+++ lib_prefab_use/src/main/cpp/src/Prefab.cpp
@@ -14,7 +14,7 @@ Java_com_peerless2012_ndk_prefab_use_PrefabTest_prefab(JNIEnv *env, jclass clazz
     std::string str = "Prefab ";
     str.append("a = " + std::to_string(a));
     str.append(", b = " + std::to_string(b));
-    str.append(", add = " + std::to_string(prefabAdd(a, b)));
-    str.append(", sub = " + std::to_string(prefabSub(a, b)));
+    str.append(", a add b = " + std::to_string(prefabAdd(a, b)));
+    str.append(", a sub b = " + std::to_string(prefabSub(a, b)));
     return env->NewStringUTF(str.c_str());
 }
